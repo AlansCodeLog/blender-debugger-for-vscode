35e8ca3c1c6187a5186307ee8da9b332828567aa
Alan <alanscodelog@gmail.com>
2018-01-20 15:28:30 -0300

    Base

---
+++ README.md
@@ -0,0 +1,91 @@
+﻿# Blender Debugger for VS Code
+
+Inspired by [Blender-VScode-Debugger](/Barbarbarbarian/Blender-VScode-Debugger) which was itself inspired by this [remote_debugger](/sybrenstuvel/random-blender-addons/blob/master/remote_debugger.py) for pycharm as explained in this [Blender Developer's Blog post](https://code.blender.org/2015/10/debugging-python-code-with-pycharm/). 
+
+
+Since the VS Code one wasn't really well documented and it looked kind of dead, once I figured it out, I was just going to add the documentation, but then I ended up rewriting the whole thing.
+
+Now it can:
+
+- Auto-detect where python is and auto set the path to ptvsd if installed.
+- Tell you when the debugger has actually attached.
+
+# How to Use
+
+I'm in the process of writing a more detailed blog post for those who just started messing with python in Blender or programming in general, but if you're semi-familiar with Python, VS Code, and the command line the following should make sense.
+
+## Installing Python and Getting PTVSD
+
+Install Python 3 with pip and check add to PATH.
+    - If you already have python installed and you can run it from the command line (aka PATH is set), the addon should find it. It uses `where python` or `whereis python` or `which python` depending on the OS to determine where python is. I only have windows at the moment, so only that is tested, but it should work.
+
+`pip install ptvsd==3.0.0"
+    - 3.0 because that was what was recommended in the VS Code documentation.
+
+## Setting up your Addon
+
+This is the most important part. Otherwise it won't work. I thought it was my VS Code config but no, it was this.
+
+In Blender go to: `User Preferences > File` and set the path to `Scripts` to the folder you're developing your addon in (e.g: "C:\Code\Blender Stuff") BUT the folder must look like this:
+
+```
+Blender Stuff
+└── addons
+    ├── your-addon-folder
+        ├── __init__.py
+        ├── ...etc
+    ├── another-addon
+    ├── ...
+```
+
+Now remove your addon from Blender if it was installed, save settings, and when you restart your addon should be installed automatically.
+
+## Setting up this Addon
+
+Install the addon.
+
+If it did not find the path it'll say "PTVSD not Found", you'll have to set it manually. It's wherever python is + "\lib\site-packages". NO trailing backslash.
+
+If you want, increase the timeout for the confirmation. It'll print "Waiting..." in the console ~ every second until it prints it's timedout. This does not mean the server has timedout *just* the confirmation listener.
+
+Open up Blender's search (default shortcut: space), type "Debug".
+
+Click `Debug: Start Debug Server for VS Code`. Note: you can only start the server once. You cannot stop it, at least from what I understand. If you run it again it'll just tell you it's already running and start the timer again to check for a confirmation.
+
+## Connecting VS Code
+
+Open your addon folder (e.g. "C:\Code\Blender Stuff\addons\myaddon").
+
+Install the Python extension for VS Code if you haven't already.
+
+Go to the Debugging tab and add a configuration. Pick Python. You'll want the configuration that looks like this, you can delete the rest. There's no need to change the defaults. 
+
+```JSON
+    {
+        "name": "Python: Attach",
+        "type": "python",
+        "request": "attach",
+        "localRoot": "${workspaceFolder}",
+        "remoteRoot": "${workspaceFolder}",
+        "port": 3000,
+        "secret": "my_secret",
+        "host": "localhost"
+    },
+```
+
+Now when you run the debugger with this config in Blender the console should print "Debugger is Attached" if it was still waiting (it should still attach even if it wasn't, it just won't tell you).
+
+## How to Use
+
+At this point you should be able to add a breakpoint and when you trigger it in Blender, Blender should freeze and VS Code should pause on the breakpoint.
+
+Note though that if you make changes to the file, Blender will not detect them. Have open `User Preferences > Addons` so you can toggle your addon on and off when you make changes. If anyone knows any way to improve this I'd love to know.
+
+# Notes
+
+The addon also detects python if PYTHONPATH is set (because Blender will add it to sys.path) or if you used the Python bundled with Blender to install ptvsd (but that's a bit of a pain because it doesn't have pip installed unless you want to install it manually)
+
+
+
+
+
---
+++ __init__.py
@@ -0,0 +1,177 @@
+bl_info = {
+   'name': 'Debugger for VS Code',
+   'author': 'Alan North',
+   'version': (0, 0, 1),
+   'blender': (2, 79, 0),
+   "description": "Starts debugging server for VS Code.",
+   'location': 'In search (default shortcut:space) type "Debug"',
+   "warning": "",
+   "wiki_url": "https://github.com/AlansCodeLog/blender-debugger-for-vscode",
+   "tracker_url": "https://github.com/AlansCodeLog/blender-debugger-for-vscode/issues",
+   'category': 'Development',
+}
+
+import bpy
+import sys
+import os
+import subprocess
+import re
+
+# check path for ptvsd
+def check_for_ptvsd():
+   #check for python with where/whereis
+   python_exe = None
+   try:
+      python_exe = subprocess.Popen(
+         ["whereis", "python"],
+         shell=False,
+         stdout=subprocess.PIPE,
+         stderr=subprocess.PIPE
+      )
+   except Exception: 
+      pass
+   if python_exe is None:
+      try: 
+         python_exe = subprocess.Popen(
+               ["where", "python"],
+               shell=False,
+               stdout=subprocess.PIPE,
+               stderr=subprocess.PIPE
+            )
+      except Exception: 
+         pass
+   if python_exe is None:
+      try: 
+         python_exe = subprocess.Popen(
+               ["which", "python"],
+               shell=False,
+               stdout=subprocess.PIPE,
+               stderr=subprocess.PIPE
+            )
+      except Exception: 
+         pass
+   if python_exe is not None:
+      python_exe = str(python_exe.communicate()[0], "utf-8")
+      match = re.search(".*(\\\\|/)", python_exe).group() 
+      if os.path.exists(match+"\lib\site-packages\ptvsd"):
+         return match+"\lib\site-packages"
+
+   #check in our path
+   for path in sys.path:
+      if os.path.exists(path+"\ptvsd"):
+         return path
+      if os.path.exists(path+"\site-packages\ptvsd"):
+         return path+"\site-packages"
+      if os.path.exists(path+"\lib\site-packages\ptvsd"):
+         return path+"lib\site-packages"
+   return "PTVSD not Found"
+
+# Preferences
+class DebuggerPreferences(bpy.types.AddonPreferences):
+   bl_idname = __name__
+
+   path = bpy.props.StringProperty(
+      name="Location of PTVSD",
+      subtype="DIR_PATH",
+      default=check_for_ptvsd()
+   )
+
+   timeout = bpy.props.IntProperty(
+      name="Timeout",
+      default=20
+   )
+   def draw(self, context):
+      layout = self.layout
+      layout.prop(self, "path")
+      layout.label(text="Pluging will try to auto-find it, if no path found, or you would like to use a different path, set it here.")
+      row = layout.split()
+      row.label(text="Timeout in seconds for attach confirmation listener.")
+      row.prop(self, "timeout")
+
+# check if debugger has attached
+def check_done(i, modal_limit):
+   if i % 100 == 0:
+      print("Waiting")
+   if i > modal_limit:
+      print("Attach Confirmation Listener Timed Out")
+      return {"CANCELLED"}
+   if not ptvsd.is_attached():
+      return {"PASS_THROUGH"}
+   print('Debugger is Attached')
+   return {"FINISHED"}
+   
+class DebuggerCheck(bpy.types.Operator):
+   bl_idname = "debug.check_for_debugger"
+   bl_label = "Debug: Check if VS Code is Attached"
+   bl_description = "Starts modal timer that checks if debugger attached until attached or until timeout."
+
+   _timer = None
+   count = 0
+   modal_limit = 20000
+
+   # call check_done
+   def modal(self, context, event):
+      self.count = self.count + 1
+      if event.type == "TIMER":
+         return check_done(self.count, self.modal_limit)
+      return {"PASS_THROUGH"} 
+
+   def execute(self, context):
+      # set initial variables
+      self.count = 0
+      prefs = bpy.context.user_preferences.addons[__name__].preferences
+      self.modal_limit = prefs.timeout*100
+
+      wm = context.window_manager
+      self._timer = wm.event_timer_add(0.1, context.window)
+      wm.modal_handler_add(self)
+      return {"RUNNING_MODAL"}
+
+   def cancel(self, context):
+      print("Debugger Confirmation Cancelled")
+      wm = context.window_manager
+      wm.event_timer_remove(self._timer)
+
+class DebugServerStart(bpy.types.Operator): 
+   bl_idname = "debug.connect_debugger_vscode"
+   bl_label = "Debug: Start Debug Server for VS Code"
+   bl_description = "Starts ptvsd server for debugger to attach to."
+   
+   def execute(self, context):
+      #get ptvsd and import if exists
+      prefs = bpy.context.user_preferences.addons[__name__].preferences
+      ptvsd_path = os.path.abspath(prefs.path)
+
+      #actually check ptvsd is still available
+      if ptvsd_path == "PTVSD not Found":
+         self.report({"ERROR"}, "Couldn't detect ptvsd, please specify the path manually in the addon preferences or reload the addon if you installed ptvsd after enabling it.")
+         return {"CANCELLED"}
+
+      if not os.path.exists(ptvsd_path+"/ptvsd"):
+         self.report({"ERROR"}, "Can't find ptvsd at: %r/ptvsd." % ptvsd_path)
+         return {"CANCELLED"}
+
+      if not any(ptvsd_path in p for p in sys.path):
+         sys.path.append(ptvsd_path)
+
+      global ptvsd #so we can do check later
+      import ptvsd
+
+      # can only be attached once, no way to detach (at least not that I understand?)
+      try:
+         ptvsd.enable_attach("my_secret", address = ("0.0.0.0", 3000))
+      except:
+         print("Server already running.")
+
+      # call our confirmation listener
+      bpy.ops.debug.check_for_debugger()
+      return {"FINISHED"}
+   
+def register():
+   bpy.utils.register_module(__name__)
+
+def unregister():
+   bpy.utils.unregister_module(__name__)
+
+if __name__ == "__main__":
+   register()

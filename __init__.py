33fc31e74b41b4812fc10e887e7c26af0294cee7
Alan <alanscodelog@gmail.com>
2018-02-13 21:19:44 -0300

    Added License
    - Fixed #2

--- __init__.py
+++ __init__.py
@@ -1,7 +1,27 @@
+'''
+Copyright (C) 2018 Alan North
+alannorth@gmail.com
+
+Created by Alan North
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 bl_info = {
    'name': 'Debugger for VS Code',
    'author': 'Alan North',
-   'version': (0, 1, 0),
+   'version': (0, 1, 1),
    'blender': (2, 79, 0),
    "description": "Starts debugging server for VS Code.",
    'location': 'In search (default shortcut:space) type "Debug"',

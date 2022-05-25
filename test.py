d60d8d13193af05db6c9ed3582b707fb95c1d3fb
Alan North <alanscodelog@gmail.com>
2022-05-25 16:12:13 -0300

    Fixed incorrect port in test script.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # addon version 2.0.0 +
 import debugpy
 
-debugpy.listen(("0.0.0.0", 3000))
+debugpy.listen(("0.0.0.0", 5678))
 print("debugpy version is: " + debugpy.__version__)
 print("Waiting")
 debugpy.wait_for_client()
